9b5ee4bc28d57121b7d91facfe1deb74e6fca6df
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2017-06-18 15:33:20 +0200

    .

--- imports/model.js
+++ imports/model.js
@@ -1,13 +1,14 @@
+import moment from 'moment';
 export const Sale = new Mongo.Collection('sales');
 
 export const searchSchema = {
-      sale_date$gte: {type: 'date', validate: (v) => v && v.isValid()},
-      sale_date$lte: {type: 'date', validate: (v) => v && v.isValid()}
+      sale_date$gte: {type: 'date', validate: (v) => v && moment(v).isValid()},
+      sale_date$lte: {type: 'date', validate: (v) => v && moment(v).isValid()}
 }
 
 export const saleSchema = {
       _id: {type: 'string'},
-      sale_date: {type: 'date', message: 'must be valid date', validate: (v) => v && v.isValid()},
+      sale_date: {type: 'date', message: 'must be valid date', validate: (v) => v && moment(v).isValid()},
       amount: {type: 'float', message: 'must be greater than 0', validate: (v) => v > 0},
       'client.value': {type: 'string'},
       'client._id': {type: 'string'},
